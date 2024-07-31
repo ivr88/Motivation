6ff9c1eb148ae4e0636294d280416bba20fc5d7b
ivr88 <ivr88@yandex.ru>
2024-07-31 17:10:05 +0300

    Initial Commit

---
+++ Motivation/MotivationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotivationApp.swift
+//  Motivation
+//
+//  Created by Вадим Исламов on 31.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct MotivationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
